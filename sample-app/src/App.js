c67843127d7c0103026782ca8fb58e0dd370c2e0
NidhitheCoder <nidhpv@gmail.com>
2021-04-05 23:20:12 +0530

    use cyclic state using use reducer- state machine introduction

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useReducer, useState } from "react";
 
-const Button = ({color,increment,underline}) => {
-  const [counter,setCounter] = useState(0)
-  return <div style={{color,textDecoration:underline&&'underline'}} onClick={()=>setCounter((c)=>c+increment)}>I am a button {counter}</div>
-}
+const reducer = ({state}) => {
+  switch (state) {
+    case "PRESSED_ONCE":
+      return { state: "PRESSED_TWO" };
+    case "PRESSED_TWO":
+      return {
+        state: "PRESSED_THREE",
+      };
+    case "PRESSED_THREE":
+      return {
+        state: "PRESSED_ONCE",
+      };
+  }
+};
+
+const Button = ({ color, increment, underline }) => {
+  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {state:"PRESSED_ONCE"});
+  return (
+    <div
+      style={{ color, textDecoration: underline && "underline" }}
+  
+    >
+    <div onClick={() => dispatch()}>I am a button</div>
+     <div>{state.state}</div>
+    </div>
+  );
+};
 
 function App() {
-  const props={
-    increment:2,
-    underline:true
-  };
-
   return (
     <div className="App">
-        <Button {...props} color="blue"/> 
-        <Button {...props} color="green"/>
-        <Button {...props} color="red"/>
+      <Button color="blue" />
+      <Button color="green" />
+      <Button color="red" />
     </div>
   );
 }
