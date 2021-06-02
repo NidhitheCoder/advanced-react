43faf5bafdc69c213c83f84e7cc01d64fb4aa0a8
NidhitheCoder <nidhpv@gmail.com>
2021-06-03 00:36:56 +0530

    React query basic fetching completed

--- react-query/react-query-app/src/App.js
+++ react-query/react-query-app/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import axios from "axios";
 
-function App() {
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-     <h1>React Query</h1>
+    <QueryClientProvider client={queryClient}>
+      <Content />
+    </QueryClientProvider>
+  );
+};
+
+const Content = () => {
+  const { isLoading, error, data } = useQuery("users", () => {
+   return axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.data)
+  });
+  
+  if (isLoading) return "Loading...";
+  if (error) return `Error occured ${error.message}`;
+  console.log(data);
+
+  return (
+    <div>
+      <h2>React Query</h2>
+      {data.map(user => <h5 key={user.id}>{user.name}</h5>)}
     </div>
   );
-}
+};
 
 export default App;
