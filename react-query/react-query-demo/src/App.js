68e8a2d2056210ff917d6e6f3c4a20f57584c8bd
NidhitheCoder <nidhpv@gmail.com>
2022-04-03 22:47:26 +0530

    feat: Router section added

--- react-query/react-query-demo/src/App.js
+++ react-query/react-query-demo/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { SuperHerosPage } from './components/Superheros.page';
+import { RQSuperHerosPage } from './components/RQSuperHeros.page';
+import { HomePage } from './components/Home.page'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="super-heros">Traditional Super Heros</Link>
+            </li>
+            <li>
+              <Link to="rq-super-heros">RQ Super Heros</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heros">
+            <SuperHerosPage />
+          </Route>
+          <Route path="/rq-super-heros">
+            <RQSuperHerosPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
