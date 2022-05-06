61916ee22c7ab7346b550ea66c2a1eb5aa605e95
NidhitheCoder <nidhpv@gmail.com>
2022-05-06 23:13:41 +0530

    feat: implement dependent query in reactQuery

--- react-query/react-query-demo/src/components/DependentQueries.page.js
+++ react-query/react-query-demo/src/components/DependentQueries.page.js
@@ -1,3 +1,21 @@
-export const DependentQueriesPage = () => {
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchUserByEmail = (email) => {
+  return axios.get(`http://localhost:4000/users/${email}`);
+}
+
+const fetchCoursesByChannelId = (channelId) => {
+  return axios.get(`http://localhost:4000/channel/${channelId}`);
+}
+
+export const DependentQueriesPage = ({ email }) => {
+
+  const { data: user } = useQuery(['user', email], () => fetchUserByEmail(email));
+  const channelId = user?.data?.channelId;
+  const { data } = useQuery(['programs', channelId], () => fetchCoursesByChannelId(channelId), {
+    enabled: !!channelId
+  })
+
   return <h1>Dependent queries</h1>
 }
