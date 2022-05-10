402a0c509e33cf46fd222c434f5479d6c68e798e
NidhitheCoder <nidhpv@gmail.com>
2022-05-11 00:21:16 +0530

    feat: add json values and show in component

--- react-query/react-query-demo/src/components/PaginatedQueries.js
+++ react-query/react-query-demo/src/components/PaginatedQueries.js
@@ -1,11 +1,33 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-export const PaginatedQueries = ({ email }) => {
+const fetchColors = () => {
+  return axios.get('http://localhost:4000/colors');
+};
+
+export const PaginatedQueries = () => {
+  const { isLoading, isError, error, data } = useQuery('colors', fetchColors);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (isError) {
+    return <h2>{error.mssage}</h2>
+  }
+
   return (
-  <div>
-    
-    <h1>Paginated queries</h1>
-  </div>
+    <div>
+      <h1>Paginated queries</h1>
+      <div>
+        {data?.data?.map((color) => {
+          return (
+            <div key={color.id}>
+              <h2>{color.id}.{color.label}</h2>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   );
 }
