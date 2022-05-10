5afbc00c89a7163f86f8a8f684eb69785484c791
NidhitheCoder <nidhpv@gmail.com>
2022-05-10 23:59:40 +0530

    feat: basic for paginated queries

---
+++ react-query/react-query-demo/src/components/PaginatedQueries.js
@@ -0,0 +1,11 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+export const PaginatedQueries = ({ email }) => {
+  return (
+  <div>
+    
+    <h1>Paginated queries</h1>
+  </div>
+  );
+}
