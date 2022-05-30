86f39ca95ef74063d2e69e46d001d185e387236c
NidhitheCoder <nidhpv@gmail.com>
2022-05-30 23:54:46 +0530

    feat: list all the albums list and navigate album details dynamically

--- router-sample-app/src/components/AlbumDetails.jsx
+++ router-sample-app/src/components/AlbumDetails.jsx
@@ -1,6 +1,24 @@
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom"
 
 export const AlbumDetails = () => {
+
+const { id } = useParams();
+
+  async function getAlbumData() {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/albums/${id}`);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAlbumData();
+  }, [getAlbumData]);
+
   return (
     <div>
       <p>Album details page</p>
--- router-sample-app/src/components/AlbumsList.jsx
+++ router-sample-app/src/components/AlbumsList.jsx
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import { useEffect } from "react";
-
-async function getUser() {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/albums');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { useState } from "react";
 
 export const AlbumsList = () => {
 
-useEffect(() => {
-getUser();
-}, [])
+  const [albums, setAlbums] = useState([]);
+
+  async function getAlbumsList() {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/albums');
+      setAlbums(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAlbumsList();
+  }, []);
+
   return (
-    <div>
+    <div className="albums-container">
       <p>Albums</p>
-      <Link to="/albums/1">First element detail</Link>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Magnam deleniti nobis distinctio incidunt, molestiae aut quidem
-        ratione animi nihil minima recusandae est nisi
-        blanditiis officiis ducimus culpa nemo in maxime.
-      </p>
       <Link to="/">Go To Home</Link>
+      <div>
+        {
+          albums.map(album => (
+            <Link className="album" to={`/albums/${album.id}`}>
+              <h3>{album.id}</h3>
+              <p>{album.title}</p>
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 }
