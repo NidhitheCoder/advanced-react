03b5c65d71ce755b11372eae1e965ceae27febe5
NidhitheCoder <nidhpv@gmail.com>
2022-05-30 23:59:45 +0530

    feat: avoid infinit api call and list data

--- router-sample-app/src/components/AlbumDetails.jsx
+++ router-sample-app/src/components/AlbumDetails.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
+import { useState } from "react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom"
 
 export const AlbumDetails = () => {
 
-const { id } = useParams();
+  const [album, setAlbum] = useState({});
+
+  const { id } = useParams();
 
   async function getAlbumData() {
     try {
       const response = await axios.get(`https://jsonplaceholder.typicode.com/albums/${id}`);
-      console.log(response);
+      setAlbum(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -17,7 +20,7 @@ const { id } = useParams();
 
   useEffect(() => {
     getAlbumData();
-  }, [getAlbumData]);
+  }, []);
 
   return (
     <div>
@@ -25,6 +28,10 @@ const { id } = useParams();
       <Link to="/albums">Back</Link>
       <p>======</p>
       <Link to="/">Go To Home</Link>
+      <div className="album-details">
+        <h3>{album?.id}</h3>
+        <p>{album.title}</p>
+      </div>
     </div>
   );
 }
