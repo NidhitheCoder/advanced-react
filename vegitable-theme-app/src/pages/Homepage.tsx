42d4cf2cc498bc22bd4113d0f99df63e60b44074
NidhitheCoder <nidhpv@gmail.com>
2022-06-11 23:53:04 +0530

    feat: map item cards with array items

--- vegitable-theme-app/src/pages/Homepage.tsx
+++ vegitable-theme-app/src/pages/Homepage.tsx
@@ -1,13 +1,18 @@
 import { Flex } from "@chakra-ui/react";
 import BeetrootComp from "../components/Beetroot";
 import BrinjalComp from "../components/Brinjal";
+import ItemCard from "../components/ItemCard";
 import OnionComp from "../components/Onion";
 import TomatoComp from "../components/Tomato";
+import { cardItems } from "../config/constants/constant";
 
 
 const Homepage = () => {
   return (
     <Flex bg="cucumbar" p={8} h="full" w="full" alignItems="center" justifyContent="space-around">
+      {cardItems?.map((item) => {
+        return <ItemCard  title={item.name} subtitle={item.description} link={item.url}/>
+      })}
       <OnionComp />
       <BeetrootComp />
       <BrinjalComp />
