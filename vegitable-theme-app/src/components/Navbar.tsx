5b0bec7959f25641dd56ff05da0be1fbb6f6bcb3
NidhitheCoder <nidhpv@gmail.com>
2022-06-17 23:33:16 +0530

    feat: styling of navbar

--- vegitable-theme-app/src/components/Navbar.tsx
+++ vegitable-theme-app/src/components/Navbar.tsx
@@ -1,19 +1,58 @@
+import { SearchIcon } from "@chakra-ui/icons";
 import { Flex, List, ListItem, Text } from "@chakra-ui/react";
-import Card from "../shared/Card";
 
 const Navbar = () => {
   return (
-    <Flex justifyContent="space-between" w="full" bg="white" py={6}>
-      <List display="flex" flexDirection="row" listStyleType="none">
-        <ListItem px={4}>Name</ListItem>
+    <Flex justifyContent="space-between" w="full" bg="orange">
+      <List
+        display="flex"
+        flexDirection="row"
+        listStyleType="none"
+        alignItems="center"
+      >
+        <ListItem px={4}>
+          <Text fontSize="xl" fontWeight="bold">
+            Company Name
+          </Text>
+        </ListItem>
         <ListItem px={4}>insights</ListItem>
         <ListItem px={4}>Services</ListItem>
         <ListItem px={4}>industries</ListItem>
       </List>
-      <Flex>
-        <Text>Search</Text>
-        <Text>SIGN IN</Text>
-        <Text>Flag</Text>
+      <Flex justifyContent="center" alignItems="center">
+        <Flex
+          p={4}
+          borderRight={1}
+          borderRightColor="gray"
+          borderRightStyle="solid"
+          borderBottom={4}
+          borderBottomColor="transparent"
+          borderBottomStyle="solid"
+          _hover={{ borderBottom: "4px solid blue" }}
+        >
+          <SearchIcon fontWeight="400" />
+        </Flex>
+        <Flex
+          p={4}
+          borderRight={1}
+          borderRightColor="gray"
+          borderRightStyle="solid"
+          borderBottom={4}
+          borderBottomColor="transparent"
+          borderBottomStyle="solid"
+          _hover={{ borderBottom: "4px solid blue" }}
+        >
+          <Text fontSize="xs">SIGN IN</Text>
+        </Flex>
+        <Flex
+          p={4}
+          borderBottom={4}
+          borderBottomColor="transparent"
+          borderBottomStyle="solid"
+          _hover={{ borderBottom: "4px solid blue" }}
+        >
+          <Text>Flag</Text>
+        </Flex>
       </Flex>
     </Flex>
   );
