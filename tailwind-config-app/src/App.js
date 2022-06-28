2f6092ed9003ce1bfd35570f73a6912320bfc683
NidhitheCoder <nidhpv@gmail.com>
2022-06-28 23:53:57 +0530

    feat: configure theme and colours

--- tailwind-config-app/src/App.js
+++ tailwind-config-app/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-400">
+    <div className="App bg-metal">
+      <h1 className="text-3xl font-bold underline tablet:text-red-400 desktop:text-green-600 text-blue-300">
         Hello world!
       </h1>
     </div>
--- tailwind-config-app/tailwind.config.js
+++ tailwind-config-app/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+        tablet: '760px',
+        desktop: '1248px',
+    },
+    colors: {
+      white: '#FFFFFF',
+      purple: '#3F3CBB',
+      midnight: '#121063',
+      metal: '#565584',
+      'tahiti-blue': '#3AB7BF',
+      'cool-white': '#ECEBFF',
+      'bubble-gum': '#FF77E9',
+      'copper-rust': '#77DCCA',
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
