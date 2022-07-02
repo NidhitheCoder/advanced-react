0209b139281e2f910c11c216096574cc1f1a32b2
NidhitheCoder <nidhpv@gmail.com>
2022-07-02 23:35:16 +0530

    feat: images added

--- tailwind-config-app/src/App.js
+++ tailwind-config-app/src/App.js
@@ -4,16 +4,25 @@ import PopupNav from './components/PopupNav';
 
 function App() {
   return (
-    <div className='h-full min-h-screen bg-dark'>
+    <div className='h-full min-h-screen bg-dark mt-16'>
       <PopupNav />
-      <div className='w-'>
+      <div className='w-96 m-auto d-flex flex-className'>
         <div className="logo">
-          
+        <div className="show-name " />
         </div>
       </div>
-      {/* <FixedBgSection
+      <div className='w-96 m-auto d-flex flex-className'>
+        <div className="logo">
+        <div className="show-name " />
+        </div>
+      </div>
+
+      <cards-container>
+        
+      </cards-container>
+      <FixedBgSection
         url="https://azurecomcdn.azureedge.net/cvt-0b95339c25de3ead5c9876ea2353921f110f3a9dfbdc97bdeca9f573aaaa2447/images/page/home/customer-tabs/forza5-desktop.jpg"
-      /> */}
+      />
     </div>
   );
 }
