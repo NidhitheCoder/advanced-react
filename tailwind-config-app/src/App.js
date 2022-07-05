3fd16933a13abc5a2176902baa58ee6a9c36b6ef
NidhitheCoder <nidhpv@gmail.com>
2022-07-05 23:43:09 +0530

    feat: pocket recommended in firefox

--- tailwind-config-app/src/App.js
+++ tailwind-config-app/src/App.js
@@ -8,6 +8,18 @@ function App() {
     <div className='h-full min-h-screen bg-dark'>
       <CustomHeader />
       <ShortCutContianer />
+      <div className='flex  w-2/4 mx-auto pt-8'>
+        <p className='text-active-color pr-2'>Recommended by Pocket</p>
+        <a href="#" className='text-white'>learn more</a>
+      </div>
+      <div className='w-2/4 mx-auto grid grid-cols-3 gap-6 py-4'>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+        <div className='w-full h-60 rounded-lg bg-light'></div>
+      </div>
       <div className='w-screen h-screen bg-dark'></div>
       <FixedBgSection
         url="https://azurecomcdn.azureedge.net/cvt-0b95339c25de3ead5c9876ea2353921f110f3a9dfbdc97bdeca9f573aaaa2447/images/page/home/customer-tabs/forza5-desktop.jpg"
--- tailwind-config-app/src/containers/ShortCutContianer.js
+++ tailwind-config-app/src/containers/ShortCutContianer.js
@@ -6,14 +6,14 @@ const ShortCutContianer = () => {
     <div className="w-2/4 bg-dark pt-72 m-auto">
       <div className="w-full min-h-36 flex flex-row justify-center gap-2">
         {shortCuts.map((shortcut) => (
-          <div className="text-center group text-white hover:bg-light p-6 rounded-md ">
+          <div className="text-center group text-white hover:bg-light px-6 py-2 rounded-md ">
             <div className="flex flex-row  w-full justify-end align-bottom pb-2" >
               <div className="h-1 w-1 group-hover:bg-white rounded-full bg-dark " />
               <div className="h-1 w-1 group-hover:bg-white rounded-full bg-dark mx-1" />
               <div className="h-1 w-1 group-hover:bg-white rounded-full bg-dark" />
             </div>
-            <div className="bg-light rounded-t-lg p-2">
-              <img className="w-16 h-16" src={shortcut.url} alt={shortcut.name} />
+            <div className="bg-dark rounded-lg opacity-80 p-2">
+              <img className="w-14 h-14 rounded-lg" src={shortcut.url} alt={shortcut.name} />
               <p>{shortcut.name}</p>
               {shortcut.sponsored && <p>{shortcut.sponsored}</p>}
             </div>
