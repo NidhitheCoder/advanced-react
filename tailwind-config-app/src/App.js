c0ce44f988b0996440d35f17e4f902d00093192f
NidhitheCoder <nidhpv@gmail.com>
2022-07-13 23:15:43 +0530

    separate component for footer

--- tailwind-config-app/src/App.js
+++ tailwind-config-app/src/App.js
@@ -4,6 +4,7 @@ import CustomHeader from './containers/CustomHeader';
 import ShortCutContianer from './containers/ShortCutContianer';
 import RecommentedList from './containers/RecommentedList';
 import { recommentedList } from './constants';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,18 +12,7 @@ function App() {
       <CustomHeader />
       <ShortCutContianer />
       <RecommentedList list={recommentedList} />
-      <div className='w-2/4 mx-auto h-screen bg-dark flex flex-row justify-between'>
-        <ul className='flex flex-row text-white list-inside'>
-          <li className='text-white pr-2 whitespace-nowrap'>Popular topics:</li>
-          <li className='text-active-color whitespace-nowrap px-2'>Self Improvement</li>
-          <li className='text-active-color whitespace-nowrap px-2'>Food</li>
-          <li className='text-active-color whitespace-nowrap px-2'>Entertainment</li>
-          <li className='text-active-color whitespace-nowrap px-2'>Health & fitness</li>
-          <li className='text-active-color whitespace-nowrap px-2'>Science</li>
-          <li className='text-active-color whitespace-nowrap px-2'>More recommendations</li>
-        </ul>
-        <p className='text-active-color whitespace-nowrap px-2'>Privacy Notice</p>
-      </div>
+      <Footer />
       <FixedBgSection
         url="https://azurecomcdn.azureedge.net/cvt-0b95339c25de3ead5c9876ea2353921f110f3a9dfbdc97bdeca9f573aaaa2447/images/page/home/customer-tabs/forza5-desktop.jpg"
       />
--- tailwind-config-app/tailwind.config.js
+++ tailwind-config-app/tailwind.config.js
@@ -48,6 +48,7 @@ module.exports = {
         'primary-color': '#fbfbfe',
         'faded-color': '#fbfbfe',
         'dark-color': '#42414d',
+        'hovered-color': 'rgba(255,255,255,0.05)'
       },
     },
   },
