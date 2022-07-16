f82b3813bdfa5a271b7db3fc540804dc2aab2762
NidhitheCoder <nidhpv@gmail.com>
2022-07-16 23:53:06 +0530

    fix: Add contents to slider

--- tailwind-config-app/src/components/PopupNav.js
+++ tailwind-config-app/src/components/PopupNav.js
@@ -4,12 +4,12 @@ const PopupNav = () => {
   const [open, setOpen] = useState(false);
   return(
     <div className='w-screen flex justify-end'>
-      <button className={`settings bg-white fill-white m-4 text-white hover:bg-light`} onClick={() => setOpen(!open)} />
-      <div className={`${open ? 'flex animate-left ' : 'hidden animate-right '} text-white w-1/5 p-8 pt-4 pr-4 h-screen bg-light flex-col `}>
-        <div className="w-full flex justify-end items-end pb-4">
+      <button className={`${open ? 'hidden' : 'block' } settings bg-white fill-white m-4 text-white hover:bg-light fixed z-40`} onClick={() => setOpen(!open)} />
+      <div className={`${open ? 'flex animate-left ' : 'animate-right '} text-white w-1/5 p-4 h-screen bg-light flex-col `}>
+        <div className="w-full flex justify-end items-end p-4 px-2">
           <button className="h-8 text-xs font-medium rounded-sm bg-hovered-color px-4" onClick={() => setOpen(false)}>Close</button>
         </div>
-        <div className="py-4 flex flex-col">
+        <div className="p-4 flex flex-col">
           <div className="flex flex-row justify-between">
             <p className="font-bold">ShortCuts</p>
             <p>toggle</p>
@@ -21,11 +21,29 @@ const PopupNav = () => {
             <option value="3">3 rows</option>
             <option value="4">4 rows</option>
           </select>
-          <input type="checkbox" className="text-white" name="Sponsored shortcuts" value="Sponsored shortcuts" />
+          <div className="flex flex-row">
+            <input type="checkbox" className="text-white mr-4" name="Sponsored shortcuts" value="Sponsored shortcuts" />
+            <span>Sponsored shortcuts</span>
+          </div>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="p-4">
+          <div className="flex flex-row justify-between py-4">
+            <h2>Recommended by Pocket</h2>
+            <p>Toggle</p>
+          </div>
+          <p>Exceptional content curated by Pocket, part of the Firefox family</p>
+        </div>
+        <div className="border-b border-gray-500 p-4">
+          <div className="flex flex-row justify-between py-4">
+            <h2>Recent activity</h2>
+            <p>Toggle</p>
+          </div>
+          <p>A selection of recent sites and content</p>
+        </div>
+        <div className="p-4 flex flex-row">
+          <p className="settings"></p>
+          <a href="someurl">Manage more settings</a>
+        </div>
       </div>
     </div>
   );
