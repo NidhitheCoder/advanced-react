1ad0d399ff311325b397e41bdae5b029f0bf2f3d
NidhitheCoder <nidhpv@gmail.com>
2022-07-17 22:07:41 +0530

    feat: inplement onclick for toggle button

--- tailwind-config-app/src/components/Toggle.js
+++ tailwind-config-app/src/components/Toggle.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const Toggle = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => {
+    setToggle(!toggle);
+  }
+
   return (
-    <div className="bg-active-color rounded-3xl w-10 px-1 h-6 flex flex-row items-center">
+    <div onClick={onToggle} className={`bg-active-color rounded-3xl w-10 px-1 h-6 flex flex-row items-center ${toggle ? 'justify-start' : 'justify-end'}`}>
       <div  className="bg-dark-color rounded-full w-4 h-4" />
     </div>
   )
