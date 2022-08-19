b4a09f50c6173e6fd3932f07425e96403b3aac81
github.com <nidhpv@gmail.com>
2022-08-19 23:57:36 +0530

    test for useeffect

--- testing-with-jest/src/components/__tests__/Sayhello.test.tsx
+++ testing-with-jest/src/components/__tests__/Sayhello.test.tsx
@@ -1,11 +1,25 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
 import SayHello from "../SayHello";
 
-test("Should render character name", () => {
-  render(<SayHello id={4} />);
-  expect(screen.getByTestId("character-show")).toBeInTheDocument();
-  expect(screen.getByTestId("character-show").textContent).toEqual("id:4");
-  const changeButton = screen.getByTestId("change-button");
-  fireEvent.click(changeButton);
-  expect(screen.getByTestId("character-show").textContent).toEqual("id:111");
+describe("something", () => {
+  let container: any;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+  test("Should render character name", () => {
+    render(<SayHello id={4} />, container);
+    expect(screen.getByTestId("character-show")).toBeInTheDocument();
+    expect(screen.getByTestId("character-show").textContent).toEqual("id:4");
+    const changeButton = screen.getByTestId("change-button");
+    fireEvent.click(changeButton);
+    expect(screen.getByTestId("character-show").textContent).toEqual("id:111");
+  });
 });
