1e370a0ad10676c5e7db6abadd5825e7c58b12aa
github.com <nidhpv@gmail.com>
2022-09-23 23:51:08 +0530

    hoc implemented

--- sample-app/src/components/HOCWrapper.tsx
+++ sample-app/src/components/HOCWrapper.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-const HOCWrapper = () => {
+const HOCWrapper = (WrappedComponent) => {
   return (
-    <div className="">
-      <p>Hello</p>
+    <div className="bg-slate-100">
+      <p className="rotate-12">One</p>
+      <p className="rotate-12">Two</p>
+      <p className="rotate-12">Three</p>
+      <p className="rotate-12">Four</p>
+      <p className="rotate-12">Five</p>
+      <p className="rotate-12">Hello</p>
+      <WrappedComponent />
     </div>
   );
 };
 
 export default HOCWrapper;
+
