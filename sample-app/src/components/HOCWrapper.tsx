2a1306bd2d446b4c4d69a4ea849c5f25d6985800
github.com <nidhpv@gmail.com>
2022-09-24 23:28:59 +0530

    feat: higher order component notes

--- sample-app/src/components/HOCWrapper.tsx
+++ sample-app/src/components/HOCWrapper.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const HOCWrapper = (WrappedComponent) => {
+  const [isLoader, setIsLoader] = useState();
+  if (isLoader) {
+    return (
+      <div className="h-screen w-screen grid place-items-center">
+        <p>Loading</p>
+      </div>
+    );
+  }
   return (
     <div className="bg-slate-100">
       <p className="rotate-12">One</p>
@@ -15,4 +24,3 @@ const HOCWrapper = (WrappedComponent) => {
 };
 
 export default HOCWrapper;
-
