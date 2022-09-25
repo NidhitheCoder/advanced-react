14c5222d0cc842d2657e48ea9bf1ac34b82ef99e
github.com <nidhpv@gmail.com>
2022-09-25 23:07:07 +0530

    feat: intital comit for calendar

---
+++ v-calendar/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="outline w-25 pa3 mr2">
+        <p className="f-red">Calender</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
