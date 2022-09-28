b335eff28b08aceb8fa59bc63a019d0f641cb09a
github.com <nidhpv@gmail.com>
2022-09-28 23:50:14 +0530

    feat: tailwind basic config set

--- v-calendar/src/App.tsx
+++ v-calendar/src/App.tsx
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex h-screen w-screen bg-linear-softgreen-softpink">
       <div className="outline w-25 pa3 mr2 text-orange-600">
         <p className="text-green-400">Calender</p>
       </div>
--- v-calendar/tailwind.config.js
+++ v-calendar/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**.{html,js, ts, jsx, tsx}"],
+  content: ["./src/**.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "linear-softgreen-softpink":
+          "linear-gradient(200deg, var(--very-soft-green) 0%, var(--soft-pink))",
+      },
+      colors: {
+        "very-soft-green":    "var(--very-soft-green)",
+        "soft-pink":  "var(--soft-pink)",
+        "strong-blue":  "var(--strong-blue)",
+        "light-grayish-blue":  "var(--light-grayish-blue)",
+        "very-soft-yellow":  "var(--very-soft-yellow)",
+        "dark-red":  "var(--dark-red)",
+        "very-dark-cyan":   "var(--very-dark-cyan)",
+        "darker-cyan":  "var(--darker-cyan)",
+        "strong-orange":  "var(--strong-orange)",
+      }
+    },
   },
   plugins: [],
 }
