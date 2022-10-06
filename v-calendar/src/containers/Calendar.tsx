c98f4f32c5fa9c1c41ce5cc184b28f5fbfaa1d10
github.com <nidhpv@gmail.com>
2022-10-06 22:35:42 +0530

    feat: create holiday list and constants file

--- v-calendar/src/containers/Calendar.tsx
+++ v-calendar/src/containers/Calendar.tsx
@@ -1,99 +1,50 @@
 import React, { useEffect, useState } from "react";
-
-const weekDay = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-const monthsData = [
-  {
-    name: "January",
-    days: 31,
-  },
-  {
-    name: "February",
-    days: 29,
-  },
-  {
-    name: "March",
-    days: 31,
-  },
-  {
-    name: "April",
-    days: 30,
-  },
-  {
-    name: "May",
-    days: 31,
-  },
-  {
-    name: "June",
-    days: 30,
-  },
-  {
-    name: "July",
-    days: 31,
-  },
-  {
-    name: "August",
-    days: 31,
-  },
-  {
-    name: "September",
-    days: 30,
-  },
-  {
-    name: "October",
-    days: 31,
-  },
-  {
-    name: "November",
-    days: 30,
-  },
-  {
-    name: "December",
-    days: 31,
-  },
-];
+import { monthsData, weekDay } from "../constants";
+import holidayList from "../data.json";
 
 const Calendar = () => {
-  const [cells, setCells] = useState<number[]>([]);
+  const [cells, setCells] = useState<Date[]>([]);
   const today = new Date(Date.now());
 
-  const getElements = (monthData: any, currentWeekDay: number) => {
+  const getElements = (monthData: any, currentYear: number, currentMonth: number) => {
     const days: any = [];
     for (let index = 0; index <= monthData.days; index++) {
-      days.push(index);
+      days.push(`${index}/${currentMonth}/${currentYear}`);
     }
     setCells(days);
   };
 
   useEffect(() => {
     const currentMonthData = monthsData[today.getMonth()];
-    getElements(currentMonthData, today.getDay());
+    getElements(currentMonthData, today.getFullYear(), today.getMonth());
   }, []);
 
   return (
     <div className="p-8">
       <div className="flex flex-row w-full justify-between text-xl font-bold">
         <p>{weekDay[today.getDay()]}</p>
-        <p>{monthsData[today.getMonth()].name}</p>
+        <div className="flex flex-row">
+          <p className="pr-4">{today.getDate()}</p>
+          <p>{monthsData[today.getMonth()].name}</p>
+        </div>
         <p>{today.getFullYear()}</p>
       </div>
       <div className="grid grid-cols-7 border-2 border-solid border-light-grayish-blue">
-        {cells.map((cell) => (
-          <div
-            key={cell}
-            className="p-4 border-2 border-solid border-light-grayish-blue text-strong-orange"
-          >
-            {cell}
-          </div>
-        ))}
+        {cells.map((cell) => {
+          console.log(cell);
+          // const isHoliday = holidayList.map(holiday => {
+          //   const currentDate = new Date(holiday.date)
+          //   console.log(currentDate, cell)
+          // })
+          return (
+            <div
+              key={cell.getDate()}
+              className="p-4 border-2 border-solid border-light-grayish-blue text-strong-orange"
+            >
+              {cell.getDate()}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
