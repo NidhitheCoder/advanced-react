0c0bd0f914e84e820982fe5736a84ddfe7067522
github.com <nidhpv@gmail.com>
2022-10-20 22:43:56 +0530

    feat: useMemo hook on form component

--- sample-app/src/components/hooks/FormComponent.tsx
+++ sample-app/src/components/hooks/FormComponent.tsx
@@ -1,33 +1,50 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const FormComponent = () => {
   const [age, setAge] = useState();
   const [name, setName] = useState();
   const [darkMode, setDarkMode] = useState();
+
+  // It changes when the name, age and darkMode changes 
+  // const person = { name, age };
+
+  // useMemo hook helps to reduce occuring each time occuration and it is consider the object is same in each render()
+  // Here only the value of person changes the age and name changed 
+  const person = useMemo(() => ({ name, age }), [age, name]);
+
+  useEffect(() => {
+    console.log(person);
+  }, [person]);
+
   return (
-    <form>
-      Age :
-      <input
-        type="number"
-        value={age}
-        onChange={(e: any) => setAge(e.target.value)}
-      />
-      <br />
-      Name :
-      <input
-        type="text"
-        value={name}
-        onChange={(e: any) => setName(e.target.value)}
-      />
-      <br />
-      <br />
-      Dark Mode:
-      <input
-        type="checkbox"
-        value={darkMode}
-        onChange={(e: any) => setDarkMode(e.target.checked)}
-      />
-    </form>
+    <div
+      className="bg-black"
+      style={{ background: darkMode ? "#333" : "#FFF" }}
+    >
+      <form>
+        Age :
+        <input
+          type="number"
+          value={age}
+          onChange={(e: any) => setAge(e.target.value)}
+        />
+        <br />
+        Name :
+        <input
+          type="text"
+          value={name}
+          onChange={(e: any) => setName(e.target.value)}
+        />
+        <br />
+        <br />
+        Dark Mode:
+        <input
+          type="checkbox"
+          value={darkMode}
+          onChange={(e: any) => setDarkMode(e.target.checked)}
+        />
+      </form>
+    </div>
   );
 };
 
