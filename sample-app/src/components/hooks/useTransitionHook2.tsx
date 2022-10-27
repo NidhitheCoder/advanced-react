2639bc979578f5fb875fd8fcc427fa4abe84a84b
github.com <nidhpv@gmail.com>
2022-10-27 22:15:40 +0530

    feat: use transition hook example 2 complete

--- sample-app/src/components/hooks/useTransitionHook2.tsx
+++ sample-app/src/components/hooks/useTransitionHook2.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, useTransition } from "react";
 
 const UseTransitionHook2 = () => {
+  const [isPending, startTransition] = useTransition();
   const [input, setInput] = useState("");
   const [list, setList] = useState<string[]>([]);
 
@@ -8,19 +9,23 @@ const UseTransitionHook2 = () => {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
-    const updatedList = [];
-    for (let index = 0; index < LIST_SIZE; index++) {
-      updatedList.push(e.target.value);
+    startTransition(() => {
+      const updatedList = [];
+      for (let index = 0; index < LIST_SIZE; index++) {
+        updatedList.push(e.target.value);
+      }
       setList(updatedList);
-    }
+    });
   };
 
   return (
     <div className="">
-      <input type="text" name="" id="" onChange={handleChange} />
-      {list.map((item, index) => {
-        return <div key={index}>{item}</div>;
-      })}
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending
+        ? "Loading..."
+        : list.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
     </div>
   );
 };
