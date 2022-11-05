7f9b23fed6f0c76e04165bc9f5ae81eb91577bb4
Nidheesh PV <nidhpv@gmail.com>
2022-11-05 23:47:24 +0530

    feat: fix styles in features section

--- react-query/react-query-app/src/components/Features.tsx
+++ react-query/react-query-app/src/components/Features.tsx
@@ -1,24 +1,35 @@
-import React from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import { useQuery } from "react-query";
 
 const Features = () => {
-  const { data: featuresList, isLoading } = useQuery('features posts', () => {
+
+  const [post, setPost] = useState<any>();
+  const { data: featuresList, isLoading } = useQuery("features posts", () => {
     return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get("https://jsonplaceholder.typicode.com/posts")
       .then((res: any) => res.data);
   });
 
   if (isLoading) {
-    return <div><p>Loading...</p></div>
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   return (
     <div>
-      <h1>Features will be listed here</h1>
-      {featuresList?.map((feature) => (
-        <div>{feature.name}</div>
-      ))}
+      <div>
+        <h1>Features will be listed here</h1>
+        {featuresList?.map((feature: any) => (
+          <div className="post">{feature.title}</div>
+        ))}
+      </div>
+      <div>
+        <p>{post.name}</p>
+      </div>
     </div>
   );
 };
