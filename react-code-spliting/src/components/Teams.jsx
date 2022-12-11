4295ab59e7923af808ace163d6076767796d5c82
Nidheesh PV <nidhpv@gmail.com>
2022-12-11 22:35:19 +0530

    feat: implement fetch with promise all

--- react-code-spliting/src/components/Teams.jsx
+++ react-code-spliting/src/components/Teams.jsx
@@ -8,29 +8,39 @@ const Teams = () => {
   const [posts, setPosts] = useState();
   const [comments, setComments] = useState();
 
-  const fetchData = async () => {
-    // const postsFromApi = await fetchPosts();
-    // const commentsFromApi = await fetchComments();
+  // const fetchData = async () => {
+  //   const postsFromApi = await fetchPosts();
+  //   const commentsFromApi = await fetchComments();
 
+  //   const postsAPI = await postsPromise;
+  //   const commentsAPI = await commentsPromise;
+
+  //   setPosts(postsAPI);
+  //   setComments(commentsAPI);
+
+  //   setPosts(postsFromApi);
+  //   setComments(commentsFromApi);
+  // };
+
+  const fetchWithPromiseAll = async () => {
     const postsPromise = fetchPosts();
     const commentsPromise = fetchComments();
+    const [posts, comments] = await Promise.all([
+      postsPromise,
+      commentsPromise,
+    ]);
 
-    const postsAPI = await postsPromise;
-    const commentsAPI = await commentsPromise;
-
-    setPosts(postsAPI);
-    setComments(commentsAPI);
-
-    // setPosts(postsFromApi);
-    // setComments(commentsFromApi);
+    setPosts(posts);
+    setComments(comments);
   };
 
   useEffect(() => {
-    fetchData();
+    // fetchData();
+    fetchWithPromiseAll();
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h3>Teams profile</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aut eaque
@@ -42,8 +52,8 @@ const Teams = () => {
 
       <h2>Posts</h2>
       {posts?.map((post) => (
-        <div style={{ padding: "2rem" }} className="p-2 text-red-50">
-          <h4>{post.title}</h4>
+        <div className="p-2 text-red-500 w-2/4 bg-slate-100 my-2 border border-1 rounded-sm border-slate-300">
+          <h4 className="text-orange-400 text-xl">{post.title}</h4>
           <p>{post.body}</p>
         </div>
       ))}
