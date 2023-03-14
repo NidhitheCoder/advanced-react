c6c7b2424285a61851cb3f5523c4da498ffc4dad
Nidheesh PV <nidhpv@gmail.com>
2023-03-14 22:45:30 +0530

    feat: implement usememo for reducing rerendering

--- react-hooks/src/components/UseEffect.js
+++ react-hooks/src/components/UseEffect.js
@@ -1,35 +1,68 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 function UseEffectHook() {
   const [number, setNumber] = useState(0);
-  // const [name, setName] = useState("");
+  const [name, setName] = useState("");
   const [userState, setUserState] = useState({
     name: "",
     selected: false,
+    age: 20,
+    city: ''
   });
 
-  useEffect(() => {
-    console.count("useEffect");
-    document.title = `Title ${number}`;
-  }, []);
+  // useEffect(() => {
+  //   console.count("useEffect");
+  //   document.title = `Title ${number}`;
+  // }, []);
 
-  console.count("Component rendered", name);
+  // console.count("Component rendered", name);
+
+  const user = useMemo(
+    () => ({
+      name: userState.name,
+      selected: useState.selected,
+    }),
+    [userState.name, userState.selected]
+  );
+
+  useEffect(() => {
+    console.log("The state has changed, useEffect runs!");
+  }, [user]);
 
   const increaseNumber = () => {
     setNumber(number + 1);
   };
 
   const onChangeName = (e) => {
-    // setName(e.target.value);
-    setUserState({ ...userState, name: e.target.value });
+    setName(e.target.value);
+  };
+
+  const addNameToUserState = () => {
+    setUserState((prev) => ({ ...prev, name: name }));
+  };
+
+  const handleSelect = () => {
+    setUserState((prev) => ({ ...prev, selected: true }));
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "20vw",
+        margin: "0 auto",
+        gap: "2rem",
+      }}
+    >
       <h3>UseEffect Hook</h3>
       <span> you clicked {number} times</span>
       <button onClick={increaseNumber}>Increase</button>
       <input onChange={onChangeName} />
+      <button onClick={addNameToUserState}>Add Name</button>
+      <button onClick={handleSelect}>Select</button>
+      <p>{`name: ${userState.name}`}</p>
+      <p>{`Selected: ${userState.selected}`}</p>
     </div>
   );
 }
