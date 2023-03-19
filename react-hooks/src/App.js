5c4f9e25e379a333174aaacd6e44501f03f3aeba
Nidheesh PV <nidhpv@gmail.com>
2023-03-19 22:15:22 +0530

    feat: impelemnt settimeout after 5 seconds using useeffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,30 @@
 // import UseEffectHook from "./components/UseEffect";
 // import UseEffectExample2 from  './components/UseEffectExample2'
 
+import { useEffect, useState } from "react";
 import UseEffectCleanup from "./components/UseEffectCleanup";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [delay, setDelay] = useState(true);
+
+  useEffect(() => {
+    if (!delay) {
+      const int = setInterval(() => setIsOpen((prev) => !prev), 500);
+
+      return () => clearInterval(int);
+    }
+  }, [delay]);
+
+  useEffect(() => {
+    const time = setTimeout(() => setDelay(false), 5000);
+    return () => clearTimeout(time);
+  }, []);
+
   return (
     <div className="App">
       <h1>React hooks</h1>
+      <p>isOpen: {isOpen.toString()}</p>
       {/* <UseEffectHook /> */}
       {/* <UseEffectExample2 /> */}
       <UseEffectCleanup />
