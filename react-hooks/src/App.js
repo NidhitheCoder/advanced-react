858bbcd326b66acc7f3ff2792e28905442241ba8
Nidheesh PV <nidhpv@gmail.com>
2023-04-05 23:58:16 +0530

    feat: update app component for try new useeffect example

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,42 +1,57 @@
-import { useEffect, useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { useState } from "react";
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import UseEffectHook from "./components/UseEffect";
-import UseEffectExample2 from "./components/UseEffectExample2";
+// import UseEffectHook from "./components/UseEffect";
+// import UseEffectExample2 from "./components/UseEffectExample2";
 
-import Home from "./components/Home";
-import Posts from "./components/Posts";
+// import Home from "./components/Home";
+// import Posts from "./components/Posts";
+
+// function App() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [delay, setDelay] = useState(true);
+
+//   useEffect(() => {
+//     if (!delay) {
+//       const int = setInterval(() => setIsOpen((prev) => !prev), 500);
+
+//       return () => clearInterval(int);
+//     }
+//   }, [delay]);
+
+//   useEffect(() => {
+//     const time = setTimeout(() => setDelay(false), 5000);
+//     return () => clearTimeout(time);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>React hooks</h1>
+//       <p>isOpen: {isOpen.toString()}</p>
+//       <UseEffectHook />
+//       <UseEffectExample2 />
+//       {/* <UseEffectCleanup /> */}
+//       <Router>
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="/posts" element={<Posts />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [delay, setDelay] = useState(true);
-
-  useEffect(() => {
-    if (!delay) {
-      const int = setInterval(() => setIsOpen((prev) => !prev), 500);
-
-      return () => clearInterval(int);
-    }
-  }, [delay]);
-
-  useEffect(() => {
-    const time = setTimeout(() => setDelay(false), 5000);
-    return () => clearTimeout(time);
-  }, []);
+  const [resourceType, setResourceType] = useState("posts");
 
   return (
-    <div className="App">
-      <h1>React hooks</h1>
-      <p>isOpen: {isOpen.toString()}</p>
-      <UseEffectHook />
-      <UseEffectExample2 />
-      {/* <UseEffectCleanup /> */}
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-        </Routes>
-      </Router>
+    <div>
+      <button onClik={() => setResourceType("posts")}>Posts</button>
+      <button onClik={() => setResourceType("users")}>Users</button>
+      <button onClik={() => setResourceType("comments")}>Comments</button>
+      <h1>{resourceType}</h1>
     </div>
   );
 }
