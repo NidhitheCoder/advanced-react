aa5538e23c32adb10fc66a45fa432c0eb3303fc0
Nidheesh PV <nidhpv@gmail.com>
2023-08-05 23:13:37 +0530

    style and css loader added

--- learn-configuration-app/src/index.js
+++ learn-configuration-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styles  from './style/style.css';
 
 // const template = React.createElement(
 //   "h1",
--- learn-configuration-app/webpack.config.js
+++ learn-configuration-app/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.css/,
+        use: ['css-loader', 'style-loader']
       }
     ]
   },
