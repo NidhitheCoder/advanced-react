e74d133b28076397ce287874e19718274c604bfe
Nidheesh PV <nidhpv@gmail.com>
2023-09-01 17:53:08 +0530

    multiple env config with webpack

--- react-ts-config/src/App.tsx
+++ react-ts-config/src/App.tsx
@@ -5,7 +5,9 @@ import Logo from "./assets/logo.svg";
 export const App = () => {
   return (
     <>
-      <h1>React typescript template</h1>
+      <h1>
+        React typescript template {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={Logo} alt="sun" height="300" width="300" />
       <img src={Image} alt="sun" height="300" width="300" />
     </>
